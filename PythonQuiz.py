928463ae226a2dd2e1134c2a35761ecb85b6a73c
Rylan Naicker <rylan.naicker@my.bdsc.school.nz>
2025-05-23 11:49:53 +1200

    simple age checker

--- PythonQuiz.py
+++ PythonQuiz.py
@@ -1,2 +1,11 @@
 
+age = input("Please enter your age: ")
 
+try:
+    age = int(age)
+    if age >= 16:
+        print("Welcome.")
+    else:
+        print("You must be at least 16 years old.")
+except ValueError:
+    print("Please enter a valid number.")
